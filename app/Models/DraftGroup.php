794cfa5b4c74ec0c0edc446b0238301bd2029c3c
Joshua Schuler <jschuler@Joshuas-MacBook-Pro.local>
2021-08-02 19:22:16 -0500

    add test cases

--- app/Models/DraftGroup.php
+++ app/Models/DraftGroup.php
@@ -25,19 +25,36 @@ class DraftGroup
     $this->draftGroupUrl = "https://www.draftkings.com/lobby/getcontests?sport={$this->sport}";
     $this->playersUrl = "https://api.draftkings.com/draftgroups/v1/draftgroups/{draft_group_id}/draftables?format=json";
     $this->gameTypeUrl = "https://api.draftkings.com/draftgroups/v1/{draft_group_id}?format=json";
+
+    $this->setDraftGroupId();
+    $this->setGameTypeId();
   }
 
   /**
-   * Set the draft group id based on type
+   * Return draft group id
    * 
-   * @return object $this
+   * @return int
+   */
+  public function getDraftGroupId()
+  {
+    return $this->draftGroupId;
+  }
+
+  /**
+   * Return players url
+   * 
+   * @return string
+   */
+  public function getPlayersUrl()
+  {
+    return $this->playersUrl;
+  }
+
+  /**
+   * Set the draft group id based on typexw
    */
   public function setDraftGroupId()
   {
-    if($this->draftGroupId > 0) {
-        return $this;
-    }
-
     $client = new \GuzzleHttp\Client();
     $response = $client->request('GET', $this->draftGroupUrl);
 
@@ -50,8 +67,6 @@ class DraftGroup
             break;
         }
     }
-
-    return $this;
   }
 
   /**
@@ -77,30 +92,22 @@ class DraftGroup
    * @param string $url
    * @return string
    */
-  protected function buildDraftGroupUrl($draftGroupId, $url)
+  public function buildDraftGroupUrl($draftGroupId, $url)
   {
     return str_replace('{draft_group_id}', $draftGroupId, $url);
   }
 
   /**
    * Set game type id
-   * 
-   * @return object $this
    */
   public function setGameTypeId()
   {
-    if($this->gameTypeId > 0) {
-      return $this;
-    }
-
     $draftGroupId = $this->getDraftGroupId();
     $gameTypeUrl = $this->buildDraftGroupUrl($draftGroupId, $this->gameTypeUrl);
     $client = new \GuzzleHttp\Client();
     $response = $client->request('GET', $gameTypeUrl);
     $gameTypeArray = json_decode($response->getBody(), true);
     $this->gameTypeId = $gameTypeArray['draftGroup']['contestType']['contestTypeId'];
-
-    return $this;
   }
 
   /**
--- tests/Unit/DraftGroupTest.php
+++ tests/Unit/DraftGroupTest.php
@@ -7,15 +7,39 @@ use App\Models\DraftGroup;
 
 class DraftGroupTest extends TestCase
 {
+    private $draftGroup;
+
     /**
      * A basic test example.
      *
      * @return void
      */
+    public function setUp(): void
+    {
+        $this->draftGroup = new DraftGroup('mlb', 'featured');
+    }
     public function test_draftgroupid()
     {
-        $draftGroup = new DraftGroup('mlb', 'featured');
-        $draftGroupId = $draftGroup->getDraftGroupId();
-        $this->assertEquals(54119, $draftGroupId);
+        $this->assertEquals(54126, $this->draftGroup->getDraftGroupId());
+    }
+
+    public function test_draftgrouprules()
+    {
+        $this->assertInstanceOf('App\Models\Rules', $this->draftGroup->getDraftGroupRules());
+    }
+
+    public function test_draftgroup_playersurl()
+    {
+        $this->assertEquals("https://api.draftkings.com/draftgroups/v1/draftgroups/{draft_group_id}/draftables?format=json", $this->draftGroup->getPlayersUrl());
+    }
+
+    public function test_draftgroup_builturl()
+    {
+        $this->assertEquals("https://api.draftkings.com/draftgroups/v1/draftgroups/54126/draftables?format=json", $this->draftGroup->buildDraftGroupUrl($this->draftGroup->getDraftGroupId(), $this->draftGroup->getPlayersUrl()));
+    }
+
+    public function test_draftgroupplayers()
+    {
+        $this->assertIsArray($this->draftGroup->getPlayersFromDraftGroup());
     }
 }
